07b2eec6537887feb06589ee4ebbd80d28d8e5b1
mauricioHidani <mhidani@student.42org.br>
2025-08-10 13:53:23 -0300

    feat: compare strings and return diff

---
+++ ft_strncmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/10 13:15:26 by mhidani           #+#    #+#             */
+/*   Updated: 2025/08/10 13:31:42 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (str1[i] && str2[i] && i < (n - 1) && str1[i] == str2[i])
+		i++;
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+}
